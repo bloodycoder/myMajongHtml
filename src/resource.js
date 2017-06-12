ffd03899b5ea05c094667ff47b6b4f980cd8074e
Picard Xie <picardxie@foxmail.com>
2017-06-12 17:01:41 +0800

    remove som
    asd

--- src/resource.js
+++ src/resource.js
@@ -6,9 +6,6 @@ var res = {
     arrow_csd: "res/arrow.json",
     mahjong_choose_csd: "res/mahjong_choose.json",
     huangzhuang_csd: "res/huangzhuang.json",
-    game_room_pad_csd: "res/game_room_pad.json",
-    game_room2_pad_csd: "res/game_room2_pad.json",
-    game_room3_pad_csd: "res/game_room3_pad.json",
     common_mask_csd: "res/common_mask.json",
     top_message_csd: "res/top_message.json",
 };
