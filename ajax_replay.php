4b3780456855901ceeac9b7b151873b179ef5e63
Picard Xie <picardxie@foxmail.com>
2017-06-15 10:30:14 +0800

    delete framework

--- ajax_replay.php
+++ ajax_replay.php
@@ -1,4 +1,9 @@
 <?php
-$ch = curl_init();
-echo("this is the infomation i get");
+$curl = curl_init();
+curl_setopt($curl,CURLOPT_URL,'http://123.207.172.12/myMajongHtml/pig.js');
+curl_setopt($curl, CURLOPT_HEADER, 0); 
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 0); 
+$data = curl_exec($curl); 
+curl_close($curl);
+echo($data);
 ?>
